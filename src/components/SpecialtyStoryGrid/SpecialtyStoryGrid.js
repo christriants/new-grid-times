f0d4b70dea9572fcca485567564eb9949f9a42ab
Chris Triantafilis <christophertriantafilis@gmail.com>
2024-11-04 17:18:38 -0500

    Exercise 4

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -6,6 +6,7 @@ import { MARKET_DATA, SPORTS_STORIES } from '../../data';
 import MarketCard from '../MarketCard';
 import SectionTitle from '../SectionTitle';
 import MiniStory from '../MiniStory';
+import { QUERIES } from '../../constants';
 
 const SpecialtyStoryGrid = () => {
   return (
@@ -47,14 +48,49 @@ const SpecialtyStoryGrid = () => {
 const Wrapper = styled.div`
   display: grid;
   gap: 48px;
+  grid-template-rows: 1fr auto;
+  grid-template-areas:
+    'market'
+    'sports';
+
+  @media ${QUERIES.tabletAndUp} {
+    grid-template-columns: 1fr auto;
+    grid-template-areas:
+      'market market'
+      'sports sports';
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-rows: 1fr auto;
+    grid-template-columns: revert;
+    grid-template-areas:
+      'market sports'
+      'market sports';
+  }
 `;
 
-const MarketsSection = styled.section``;
+const MarketsSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  grid-area: market;
+`;
 
-const MarketCards = styled.div``;
+const MarketCards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));
+  gap: 16px;
+`;
 
-const SportsSection = styled.section``;
+const SportsSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  grid-area: sports;
+`;
 
-const SportsStories = styled.div``;
+const SportsStories = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));
+  gap: 16px;
+`;
 
 export default SpecialtyStoryGrid;
