ec877529641f46db6e92da23e035609495814ab1
Chris Triantafilis <christophertriantafilis@gmail.com>
2024-11-05 10:10:50 -0500

    Exercise 5

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ import styled from 'styled-components/macro';
 import MaxWidthWrapper from '../MaxWidthWrapper';
 
 import VisuallyHidden from '../VisuallyHidden';
+import { QUERIES } from '../../constants';
 
 const Footer = () => {
   return (
@@ -144,6 +145,18 @@ const TopRow = styled.div`
   font-size: 0.875rem;
   border-bottom: 1px solid var(--color-gray-700);
   padding: 24px 0;
+
+  @media ${QUERIES.tabletAndUp} {
+    flex-direction: row;
+    gap: 48px;
+    justify-content: center;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    flex-direction: row;
+    gap: 48px;
+    justify-content: flex-end;
+  }
 `;
 
 const Social = styled.div`
@@ -170,6 +183,13 @@ const MainNavArea = styled.div`
   gap: 32px;
   padding: 32px 0 48px;
   text-align: center;
+
+  @media ${QUERIES.tabletAndUp} {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 250px);
+    align-content: start;
+    text-align: left;
+  }
 `;
 
 const MainNavHeading = styled.h2`
@@ -196,6 +216,10 @@ const Subfooter = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  @media ${QUERIES.laptopAndUp} {
+    align-items: start;
+  }
 `;
 
 const Logo = styled.a`
